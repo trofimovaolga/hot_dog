b35234152546f7f5f08bb633eda95c94fb7edfd3
Olga Trofimova <trofimova.olya@mail.ru>
2022-02-01 15:15:16 +0300

    param as in the report

--- prep_dataset.py
+++ prep_dataset.py
@@ -7,7 +7,7 @@ import zipfile, io
 import config
 
 random.seed(config.seed)
-num_files_to_move = 150
+num_files_to_move = 200
 
 def load_data():
     base_url = 'https://storage.googleapis.com/kaggle-data-sets/8552/57440/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20220131%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20220131T190927Z&X-Goog-Expires=259199&X-Goog-SignedHeaders=host&X-Goog-Signature=22bca864a8f8365afd3586b8a442fe999ec251b536eff32b66512c66a9ea9dbcfe4c628a358e29d09c0214425fd9419e322e45816bb61e6e11b60b0ee7eaea5c817e0062331a1e27f8dffce03acb4222a094ee8d6aa377672d3db2cdab12936663eb6678f451742410f74e3fe14af0f666e29dc9857092124a09bec3aacab206050e19ec6fe1cd7e2a0efd75d8157dd37a0d25a8c0bbc30e21ace53650dcff88c346a8611c0b9d116f030afb6b415fc88fac59fc12608b9042e47031aad8cc5d7c0dc1c5a3fa1622188e569f850a5947048f6f1497ece71d25e80b5e1090979029ee9bee190d5ebf9ac0efe2d2e3943caf1a86aa5b7c908eb9fb8dfe39bc687e'
